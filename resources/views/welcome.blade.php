cde164a8e76a3796f3f10bbb5c27bff029520d41
Humberto <humbertobb0101@gmail.com>
2017-05-11 17:30:43 -0600

    cambio temporal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-
+<!--nada por comitiar  -->
     <title>Volkswagen EuroCity</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset("css/bootstrap.css")}}">
